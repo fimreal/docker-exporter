be4879d273e37af347d362ba5629607c88451911
fimreal <lmr@epurs.com>
2026-03-13 20:11:23 +0800

    Add Docker API version auto-negotiation support

--- dockercli/client.go
+++ dockercli/client.go
@@ -13,8 +13,19 @@ type DockerClient struct {
 }
 
 // NewCli 创建新的 DockerClient 实例
+// 如果 clientVersion 为空，则使用自动版本协商
 func NewCli(dockerHost, clientVersion string) (*DockerClient, error) {
-	cli, err := client.NewClientWithOpts(client.WithHost(dockerHost), client.WithVersion(clientVersion))
+	opts := []client.Opt{client.FromEnv, client.WithHost(dockerHost)}
+
+	// 只有指定了版本时才设置版本，否则使用自动协商
+	if clientVersion != "" {
+		opts = append(opts, client.WithVersion(clientVersion))
+	} else {
+		// 使用自动版本协商
+		opts = append(opts, client.WithAPIVersionNegotiation())
+	}
+
+	cli, err := client.NewClientWithOpts(opts...)
 	if err != nil {
 		return nil, err
 	}
